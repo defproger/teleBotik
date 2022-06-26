2a765a0306292aa389d70579e8c510ad46ca2cab
defproger <defproger@gmail.com>
2022-06-27 02:48:16 +0300

    стандартный бот

--- index.php
+++ index.php
@@ -30,77 +30,50 @@ $bot->getUpdate(
     function ($text) use ($bot) {
         switch ($text) {
             case '/start':
-                $bot->Message('И тебе привет, отправь номер')
+                $bot->Message('Привет, вот функционал')
                     ->replyMarkup(
-                        Msg::getContact()
+                        Msg::Inline(
+                            Msg::Row(
+                                Msg::dataBtn('кнопочки', 'hellobtn'), Msg::dataBtn('иди нахуй', 'fuckOff')
+                            ),
+                            Msg::Row(
+                                Msg::dataBtn('верификация', 'contact')
+                            )
+                        )
                     )
                     ->Send();
                 break;
         }
     },
     function ($data) use ($bot) {
-
+        switch ($data) {
+            case 'hellobtn':
+                $bot->Message('Снизу появились кнопочки')
+                    ->replyMarkup(
+                        Msg::Keyboard(
+                            Msg::Row(
+                                Msg::Key('Приветь'), Msg::Key('Приветь2')
+                            )
+                        )
+                    )
+                    ->Send();
+                break;
+            case 'fuckOff':
+                $bot->Message('Сам иди нахуй')
+                    ->Send();
+                break;
+            case 'contact':
+                $bot->Message('Для верификации отправьте контакт')
+                    ->replyMarkup(
+                        Msg::getContact()
+                    )
+                    ->Send();
+                break;
+        }
     }
 );
 
 $bot->endLog();
-
-exit();
-//$command = [
-//    "inlineMenu" => [
-//        'reload' => [
-//            "data" => "reload",
-//            "text" => "обновить",
-//            'type' => 'data'
-//        ],
-//    ],
-//    "cmd" => [
-//        "info" => "Смотреть информацию",
-//        "reg" => "Регистрировать информацию",
-//    ]
-//];
-////все нажатия на кнопки
-//if (isset($update['callback_query'])) {
-//    include_once "components/callback_data.php";
-//}
-////при тексте
-//$queryType = 'message';
-//$msg = $update['message']['text'];
-//$user_id = intval($update['message']['chat']['id']);
-//
-////$f = fopen('./.history', 'a+');
-////fputs($f, date('Y-m-d H:i:s') . " | user: {$user_id} | massage: {$msg} |\n-------------------------\n");
-////fclose($f);
-//
-//switch ($msg) {
-//    case 'q':
-//        break;
-//    case '/start':
-//        sendMassage([
-//            'text' => "Выбери нужную функцию в меню снизу",
-//            'buttonCountLine' => 2,
-//            'bottomKeyboard' => [$command['cmd']['info'], $command['cmd']['reg']]
-//        ]);
-//        break;
-//    case $command["cmd"]['info']:
-//
-//        sendMassage($searchParametresInfo);
-//        db_update($tgUserstable, $user_id, ['type' => 'search']);
-//
-//        break;
-//
-//    default:
-//
 //        if (isset($update['message']['contact'])) {
-//            sendMassage([
-//                'text' => "Запрос на авторизацию\n" . $nic['login'] . ' ' . $update['message']['contact']['phone_number'],
-//                'chat' => $admin,
-//                'API_TOKEN' => $botToken, // токен nyana_bot
-//                'inline_keyboard' => toKeyboard(['text' => 'Разрешить', 'data' => 'addUser_' . $user_id . '_' . $admin]),
-//                'inlineData' => 1,
-//            ]);
-//        }
-//
-//        break;
-//}
+
 
